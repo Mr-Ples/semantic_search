517ae77fc45ccd667d6dbf1bfb39f5ec80543e20
simon <simonsingularitygroup@gmail.com>
2023-05-14 14:41:39 +0200

    add fixes

--- drive_downloader.py
+++ drive_downloader.py
@@ -26,91 +26,93 @@ def download_documents_from_folder(service, folder_id, folder_path=''):
     results = service.files().list(q=query, fields="nextPageToken, files(id, name, mimeType)").execute()
     items = results.get('files', [])
     folder_path = "/".join([part.strip() for part in folder_path.split('/')])
+    print("folder path:", folder_path)
     os.makedirs(folder_path, exist_ok=True)
     print(items)
+
     if not items:
         print('No files found.')
-    else:
-        for item in items:
-            try:
-                print()
-                with open(WHITELIST, 'r') as ye:
-                    wl = [line.strip() for line in ye.readlines()]
-                item_id = item['id']
-                print("folder path:", folder_path)
-                item_name = item['name'].strip() + '--' + item_id
-                item_type = item['mimeType']
-                print("item type:", item_type)
-                item_path = os.path.join(folder_path, item_name)
+        return
 
-                print("item_path:", item_path)
-                if any([folder_path in wl_folder for wl_folder in WL_FOLDERS]):
-                    print('Whitelisted')
-                    continue
-                if os.path.isfile(item_path + '.pdf'):
-                    continue
-                print("wl:", wl)
-                if item_path + '--' in wl:
-                    continue
-
-                # Retrieve file metadata, including file size
-                file_metadata = service.files().get(fileId=item_id, fields='size').execute()
-                file_size = int(file_metadata.get('size', 0))
-
-                if item_type == 'application/vnd.google-apps.folder':
-                    print(f"Folder: {item_path}")
-                    download_documents_from_folder(service, item_id, folder_path=item_path)
-
-                # Check if the file is a Google Docs file
-                if not item_type.startswith('application/vnd.google-apps'):
-                    continue
-                print(f"File: {item_path} ({item_type})")
-                # Define the export size limit (e.g., 50 MB for Google Docs)
-                export_size_limit = 50 * 1024 * 1024
-
-                # Check if the file size is within the export limit
-                if file_size >= export_size_limit:
-                    print(f"Error: The file '{item_name}' is too large to be exported.")
-                    with open(WHITELIST, 'a+') as yeet:
-                        yeet.write("\n" + item_path.replace(".pdf", '') + '.pdf')
-                    continue
-
-                # Export Google Docs file as PDF
-                request = service.files().export_media(fileId=item_id, mimeType='application/pdf')
-                if item_type == 'application/vnd.google-apps.spreadsheet':
-                    continue
-                    item_path = os.path.splitext(item_path)[0] + '--spreadsheets' + '.pdf'
-                else:
-                    item_path = os.path.splitext(item_path)[0] + '--document' + '.pdf'
+    for item in items:
+        try:
+            print()
+            with open(WHITELIST, 'r') as ye:
+                wls = [line.strip() for line in ye.readlines()]
+            print('wls:', wls)
+            item_id = item['id']
+            item_type = item['mimeType']
+            if not item_type.startswith('application/vnd.google-apps'):
+                print("File not a doc!")
                 # # Download the file directly
                 # request = service.files().get_media(fileId=item_id)
-                fh = io.BytesIO()
-                downloader = MediaIoBaseDownload(fh, request, chunksize=10 * 1024 * 1024)
-                done = False
-                while done is False:
-                    status, done = downloader.next_chunk()
-                    print("Download %d%%." % int(status.progress() * 100))
+                continue
+            if item_type == 'application/vnd.google-apps.spreadsheet':
+                print("Skipping spreadsheets")
+                continue
+            if item_type == 'application/vnd.google-apps.folder':
+                item_path = item['name'].strip() + '--' + item_id
+                print("Its a folder. Going deeper.")
+                print(f"item_path:", item_path)
+                download_documents_from_folder(service, item_id, folder_path=item_path)
+                continue
 
-                fh.seek(0)
-                print(folder_path)
-                print(item_path)
-                with open(item_path, 'wb+') as f:
-                    shutil.copyfileobj(fh, f)
+            url_type = '--document' if item_type == 'application/vnd.google-apps.document' else '--spreadsheets'
+            item_name = item['name'].strip() + '--' + item_id + '--' + url_type + '.pdf'
+            print("url_type:", url_type)
+            print("item_type:", item_type)
+            print("item_name:", item_name)
+            item_path = os.path.join(folder_path, item_name)
+            print("item_path:", item_path)
+            if any([wl_folder.lower() in [elem.strip().lower() for elem in item_path.split('/')] for wl_folder in WL_FOLDERS]):
+                print('Whitelisted folder')
+                continue
+            if os.path.isfile(item_path):
+                print("File exists!")
+                continue
+            if any([item_path in wl for wl in wls]):
+                print('Whitelisted File!')
+                continue
 
-                    print("File downloaded")
-            except HttpError as error:
-                error_details = json.loads(error.content.decode('utf-8'))
-                error_reason = error_details['error']['errors'][0]['reason']
-                if error_reason == 'exportSizeLimitExceeded':
-                    print(f"Error: The file '{item_name}' is too large to be exported.")
-                    with open(WHITELIST, 'a+') as yeet:
-                        yeet.write("\n" + item_path.replace(".pdf", '') + '.pdf')
-                else:
-                    print(f"Error: {error}")
-            except:
-                traceback.print_exc()
+            # Retrieve file metadata, including file size
+            file_metadata = service.files().get(fileId=item_id, fields='size').execute()
+            file_size = int(file_metadata.get('size', 0))
+            # Define the export size limit (e.g., 50 MB for Google Docs)
+            export_size_limit = 50 * 1024 * 1024
+            # Check if the file size is within the export limit
+            if file_size >= export_size_limit:
+                print(f"Error: The file '{item_name}' is too large to be exported.")
                 with open(WHITELIST, 'a+') as yeet:
-                    yeet.write("\n" + item_path.replace(".pdf", '') + '.pdf')
+                    yeet.write("\n" + item_path)
+                continue
+
+            # Export Google Docs file as PDF
+            request = service.files().export_media(fileId=item_id, mimeType='application/pdf')
+            fh = io.BytesIO()
+            downloader = MediaIoBaseDownload(fh, request, chunksize=10 * 1024 * 1024)
+            done = False
+            while done is False:
+                status, done = downloader.next_chunk()
+                print("Download %d%%." % int(status.progress() * 100))
+            fh.seek(0)
+            print(folder_path)
+            print(item_path)
+            with open(item_path, 'wb+') as f:
+                shutil.copyfileobj(fh, f)
+                print("File pdf export downloaded!")
+        except HttpError as error:
+            error_details = json.loads(error.content.decode('utf-8'))
+            error_reason = error_details['error']['errors'][0]['reason']
+            if error_reason == 'exportSizeLimitExceeded':
+                print(f"Error: The file '{item_name}' is too large to be exported.")
+                with open(WHITELIST, 'a+') as yeet:
+                    yeet.write("\n" + item_path)
+            else:
+                print(f"Error: {error}")
+        except:
+            traceback.print_exc()
+            with open(WHITELIST, 'a+') as yeet:
+                yeet.write("\n" + item_path)
 
 
 def get_header_data(doc_id: str):
